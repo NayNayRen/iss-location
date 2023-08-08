eb0071aab5e088a2806a91c201b84dcaa22f4e50
Nate <jeditendencies35@gmail.com>
2023-08-08 16:28:21 -0400

    initial commit of iss location

---
+++ scripts.js
@@ -0,0 +1,27 @@
+let layer = L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png');
+let map = new L.map('map', {
+  center: [0, 0],
+  zoom: 2,
+  scrollWheelZoom: false
+});
+
+async function getLocation() {
+  let res = await fetch('http://api.open-notify.org/iss-now.json');
+  let data = await res.json();
+  return data;
+}
+
+async function getISSLocation() {
+  const data = await getLocation();
+  let latitude = data.iss_position.latitude;
+  let longitude = data.iss_position.longitude;
+  let marker = L.marker([latitude, longitude]).bindPopup('ISS Location').openPopup();
+  map.setView([latitude, longitude]);
+  marker.addTo(map);
+  layer.addTo(map);
+
+  setInterval(getISSLocation, 35000);
+}
+
+
+window.onload = getISSLocation;
