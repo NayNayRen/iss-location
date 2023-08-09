ee361f4be2f29f276a940eea26d72f443cd07314
Nate <jeditendencies35@gmail.com>
2023-08-08 22:20:10 -0400

    trying to fix the mixed content error

--- scripts.js
+++ scripts.js
@@ -5,21 +5,40 @@ let map = new L.map('map', {
   scrollWheelZoom: false
 });
 
-async function getLocation() {
-  let res = await fetch('http://api.open-notify.org/iss-now.json');
-  let data = await res.json();
-  return data;
+function getLocation() {
+  // let res = await fetch('http://api.open-notify.org/iss-now.json');
+  fetch("http://api.open-notify.org/iss-now.json", {
+    headers: { Origin: window.location.host }
+  })
+    .then(res => res.json())
+    .then(res => {
+      // console.log(res);
+      let latitude = res.iss_position.latitude;
+      let longitude = res.iss_position.longitude;
+      let marker = L.marker([latitude, longitude]).bindPopup('ISS Location').openPopup();
+      map.setView([latitude, longitude]);
+      marker.addTo(map);
+      layer.addTo(map);
+      setInterval(getLocation, 5000);
+    })
+    .catch(err => {
+      console.log(err);
+    });
+  // let data = await res.json();
+  // return data;
 }
 
-async function getISSLocation() {
-  let data = await getLocation();
-  let latitude = data.iss_position.latitude;
-  let longitude = data.iss_position.longitude;
-  let marker = L.marker([latitude, longitude]).bindPopup('ISS Location').openPopup();
-  map.setView([latitude, longitude]);
-  marker.addTo(map);
-  layer.addTo(map);
-  setInterval(getISSLocation, 35000);
-}
+// async function getISSLocation() {
+//   let data = await getLocation();
+//   let latitude = data.iss_position.latitude;
+//   let longitude = data.iss_position.longitude;
+//   let marker = L.marker([latitude, longitude]).bindPopup('ISS Location').openPopup();
+//   map.setView([latitude, longitude]);
+//   marker.addTo(map);
+//   layer.addTo(map);
+//   setInterval(getISSLocation, 35000);
+// }
 
-window.onload = getISSLocation;
+// window.onload = getISSLocation;
+window.onload = getLocation;
+
