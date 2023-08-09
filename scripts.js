3d5c48f93cde894be6dfd736fcfd5c80833aa4a6
Nate <jeditendencies35@gmail.com>
2023-08-09 00:20:11 -0400

    display, timing, and background worked out, icon added

--- scripts.js
+++ scripts.js
@@ -1,9 +1,22 @@
+// set marker to null for easy removal
+let marker = null;
+let testMarker = L.marker([0, 0]);
 let layer = L.tileLayer('https://{s}.tile.osm.org/{z}/{x}/{y}.png');
 let map = new L.map('map', {
   center: [0, 0],
   zoom: 2,
   scrollWheelZoom: false
 });
+const issIcon = L.icon({
+  iconUrl: "../img/iss.png",
+  iconSize: [40, 32],
+  // [x, y] axis movements
+  iconAnchor: [20, 20],
+  popupAnchor: [0, -10]
+});
+const popup = L.popup()
+  .setContent(`<p>Satelite: ISS<br />This is a nice popup.</p>`);
+
 
 async function getLocation() {
   let res = await fetch('https://api.wheretheiss.at/v1/satellites/25544');
@@ -11,16 +24,21 @@ async function getLocation() {
   return data;
 }
 
-async function getISSLocation() {
+async function showLocation() {
   let data = await getLocation();
   // console.log(data);
   let latitude = data.latitude;
   let longitude = data.longitude;
-  let marker = L.marker([latitude, longitude]).bindPopup('ISS Location').openPopup();
+  if (marker !== null) {
+    map.removeLayer(marker);
+  }
+  marker = L.marker([latitude, longitude], {
+    icon: issIcon
+  }).bindPopup(popup).openPopup();
   map.setView([latitude, longitude]);
   marker.addTo(map);
   layer.addTo(map);
 }
-setInterval(getISSLocation, 35000);
+setInterval(showLocation, 5000);
 
-window.onload = getISSLocation;
+window.onload = showLocation;
