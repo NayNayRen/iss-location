b2f02c62e6d67dcc092054abc9658910b14ea8d3
Nate <jeditendencies35@gmail.com>
2023-08-15 14:09:36 -0400

    only 2 points kept, 1 collection kept, can switch from mi to km data too

--- scripts.js
+++ scripts.js
@@ -10,6 +10,7 @@ const mapDataButton = document.querySelector('.map-data-button');
 const latDirection = document.querySelector('.lat-direction');
 const lngDirection = document.querySelector('.lng-direction');
 const mapDataContainer = document.querySelector('.map-data-container');
+const mapDataCheckbox = document.querySelector('.map-data-checkbox');
 const currentDate = document.querySelector('.current-date');
 const currentTime = document.querySelector('.current-time');
 const positionGroups = [];
@@ -21,7 +22,7 @@ let map = new L.map('map', {
   dragging: false,
   scrollWheelZoom: false,
   trackResize: true,
-  zoom: 3
+  zoom: 4
 });
 const issIcon = L.icon({
   iconUrl: "img/iss.png",
@@ -34,6 +35,28 @@ const issIcon = L.icon({
 function kilometersToMiles(kilometers) {
   let miles = kilometers / 1.6093440006147;
   return miles;
+}
+
+async function changeToKilometers() {
+  let data = await getStationData();
+  let height = Math.round(kilometersToMiles(data.altitude) * 1) / 1;
+  let speed = Math.round(kilometersToMiles(data.velocity) * 1) / 1;
+  if (mapDataCheckbox.checked) {
+    altitudeData.innerText = `${Math.round(data.altitude * 1) / 1} km`;
+    velocityData.innerText = `${(Math.round(data.velocity) * 1 / 1).toLocaleString('en-US')} k/h`;
+  } else {
+    altitudeData.innerText = `${height} mi`;
+    velocityData.innerText = `${speed.toLocaleString('en-US')} m/h`;
+  }
+}
+
+// show and hide up arrow
+function activateUpArrow() {
+  if (document.documentElement.scrollTop > 0) {
+    upArrow.style.right = "-2px";
+  } else {
+    upArrow.style.right = "-60px";
+  }
 }
 
 async function getStationData() {
@@ -55,30 +78,39 @@ async function showStationData() {
     stroke: true,
     weight: 2
   });
-
+  // keeps two points in order to draw a line
   positions.push(new L.LatLng(latitude, longitude));
   if (positions.length > 2) {
     positions.shift();
-    // console.log(positions);
   }
+  // keeps one collection of coordinates to not lag
   positionGroups.push(positions);
-  // negative is south posative is north
+  if (positionGroups.length > 1) {
+    positionGroups.shift();
+  }
+  // negative is south positive is north
   if (latitude > 0) {
     latDirection.innerText = 'N';
   } else {
     latDirection.innerText = 'S';
   }
-  // negative is west posative is east
+  // negative is west positive is east
   if (longitude > 0) {
     lngDirection.innerText = 'E';
   } else {
     lngDirection.innerText = 'W';
   }
+  // switches between miles and kilometers
+  if (mapDataCheckbox.checked) {
+    altitudeData.innerText = `${Math.round(data.altitude * 1) / 1} km`;
+    velocityData.innerText = `${(Math.round(data.velocity) * 1 / 1).toLocaleString('en-US')} k/h`;
+  } else {
+    altitudeData.innerText = `${height} mi`;
+    velocityData.innerText = `${speed.toLocaleString('en-US')} m/h`;
+  }
   // iss data gets displayed
   latitudeData.innerText = latitude;
   longitudeData.innerText = longitude;
-  altitudeData.innerText = height;
-  velocityData.innerText = speed.toLocaleString('en-US');
   currentDate.innerText = now.toLocaleDateString();
   currentTime.innerText = now.toLocaleTimeString();
   // if marker exists, remove it, used to show only one marker
@@ -92,7 +124,6 @@ async function showStationData() {
     title: 'ISS Icon'
   });
   // layers are added to the map
-  // map.setView([latitude, longitude]);
   map.panTo([latitude, longitude]);
   marker.addTo(map);
   layer.addTo(map);
@@ -101,20 +132,15 @@ async function showStationData() {
 // function is called every 2 seconds to mimic station movement
 setInterval(showStationData, 2000);
 
-// show and hide up arrow
-function activateUpArrow() {
-  if (document.documentElement.scrollTop > 0) {
-    upArrow.style.right = "-2px";
-  } else {
-    upArrow.style.right = "-60px";
-  }
-}
-
 window.addEventListener('load', () => {
   showStationData();
   activateUpArrow();
+  mapDataCheckbox.checked = false;
   mapDataButton.addEventListener('click', () => {
     mapDataContainer.classList.toggle('map-data-container-toggle');
+  });
+  mapDataCheckbox.addEventListener('click', () => {
+    changeToKilometers();
   });
 });
 
